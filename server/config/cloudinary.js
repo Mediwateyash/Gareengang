5a57c7f675705dcdd631f17ca6700f5f37c76186
mediwateyash <diwateyash2004@gmail.com>
2026-02-09 21:29:37 +0530

    fix: Restore corrupted Cloudinary config

---
+++ server/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'divhwv5vp',
+    api_key: '834343362116733',
+    api_secret: 'm8w3KjNvkxvoKX2rHxynScG4Zsg'
+});
+
+module.exports = cloudinary;
